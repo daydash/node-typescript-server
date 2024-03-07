a3ebd28870936a43196a3041f360cf9dee42d68f
daydash <thedaydash@gmail.com>
2024-03-07 15:09:35 +0530

    updates

--- src/db/users.ts
+++ src/db/users.ts
@@ -27,3 +27,17 @@ const UserSchema = new mongoose.Schema({
 });
 
 export const UserModel = mongoose.model("User", UserSchema);
+
+export const getUsers = async () => await UserModel.find();
+export const getUserByEmail = async (email: string) =>
+	await UserModel.findOne({ email });
+export const getUserBySessionToken = async (sessionToken: string) =>
+	await UserModel.findOne({ "authentication.sessionToken": sessionToken });
+export const getUserById = async (id: string) =>
+	await UserModel.findOne({ id });
+export const createUser = async (values: Record<string, any>) =>
+	new UserModel(values).save().then((user) => user.toObject());
+export const deleteUserById = async (id: string) =>
+	await UserModel.findOneAndDelete({ id });
+export const updateUserById = async (id: string, values: Record<string, any>) =>
+	await UserModel.findByIdAndUpdate(id, values);
